2f5a331f773afd084ceef600a181ed10c7583e10
surranc20 <surranc20@mail.wlu.edu>
2020-07-05 14:36:25 -0500

    formatting

--- src/App.js
+++ src/App.js
@@ -7,7 +7,9 @@ function App() {
 
   return (
     <div className="App">
-      <SideBar tileSets={tileSets} updateTileSets={updateTileSets}/>
+      <SideBar
+        tileSets={tileSets}
+        updateTileSets={updateTileSets}/>
     </div>
   );
 }
