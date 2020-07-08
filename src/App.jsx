e2f61eb3c700b6aaed7bc4034c61491828840476
surranc20 <surranc20@mail.wlu.edu>
2020-07-07 23:40:55 -0500

    App file now passes linter

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import React, { useState } from 'react';
+import Grid from '@material-ui/core/Grid';
+import SideBar from './components/SideBar';
+import TileMap from './components/TileMap';
+import './App.css';
+
+function App() {
+  const [tileSets, updateTileSets] = useState([]);
+  return (
+    <div className="App">
+      <h1>Pokemon Tile Map Maker</h1>
+      <Grid container spacing={3}>
+        <Grid item sm={4}>
+          <SideBar
+            tileSets={tileSets}
+            updateTileSets={updateTileSets}
+          />
+        </Grid>
+        <Grid item sm={8}>
+          <TileMap tileSize={[16, 16]} />
+        </Grid>
+      </Grid>
+    </div>
+  );
+}
+
+export default App;
