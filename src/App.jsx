dbdf32d47bd75b155b543664947132382fd39f5a
surranc20 <surranc20@mail.wlu.edu>
2020-07-09 00:21:48 -0500

    Changed grid alignment and changed tile map name

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,16 @@
 import React, { useState } from 'react';
 import Grid from '@material-ui/core/Grid';
 import SideBar from './components/SideBar';
-import TileMap from './components/TileMap';
+import TileMapSpot from './components/TileMap';
 import './App.css';
 
 function App() {
   const [tileSets, updateTileSets] = useState([]);
+
   return (
     <div className="App">
       <h1>Pokemon Tile Map Maker</h1>
-      <Grid container spacing={3}>
+      <Grid container spacing={3} alignItems="center">
         <Grid item sm={4}>
           <SideBar
             tileSets={tileSets}
@@ -17,7 +18,7 @@ function App() {
           />
         </Grid>
         <Grid item sm={8}>
-          <TileMap tileSize={[16, 16]} />
+          <TileMapSpot />
         </Grid>
       </Grid>
     </div>
